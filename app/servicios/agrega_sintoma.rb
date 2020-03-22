4e87930872f0abb2e95eda7a5032a0090f84bdb7
Alexander <go.alectrico@gmail.com>
2020-03-22 07:32:17 -0300

    AgregaSintoma

--- app/servicios/agrega_sintoma.rb
+++ app/servicios/agrega_sintoma.rb
@@ -7,7 +7,7 @@ class AgregaSintoma < AgregaCarga
     nombre = NOMBRE_SINTOMAS
     imagen = IMAGEN_SINTOMAS
 
-    index = ID_SINTOMAS.find_index(un_id)
+    index = ID_SINTOMAS.find_index(un_id.to_i)
     if index > 0
       equipo = Mock::TipoEquipo.new( un_id, NOMBRE_SINTOMAS[index], IMAGEN_SINTOMAS[index] )
       
