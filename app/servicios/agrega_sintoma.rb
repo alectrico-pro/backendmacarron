fe5fb8ba545b784760fa76bf3a576055509c555b
Alexander <go.alectrico@gmail.com>
2020-03-22 07:28:40 -0300

    De nuevo

--- app/servicios/agrega_sintoma.rb
+++ app/servicios/agrega_sintoma.rb
@@ -3,10 +3,14 @@ class AgregaSintoma < AgregaCarga
   #Esta calse redefine get_equipo_con_image_url para agregar síntomas en lugar de cargas
   def get_equipo_con_image_url un_id
     linea.info "Encontrando equipo"
-    linea.info "id es #{un_id}"
-    ID_SINTOMAS.select{|id| id == un_id.to_i}.each_with_index do |id, index |
-      equipo = Mock::TipoEquipo.new( id, NOMBRE_SINTOMAS[index], IMAGEN_SINTOMAS[index] )
-      raise equipo.inspect
+    id     = ID_SINTOMAS
+    nombre = NOMBRE_SINTOMAS
+    imagen = IMAGEN_SINTOMAS
+
+    index = ID_SINTOMAS.find_index(un_id)
+    if index > 0
+      equipo = Mock::TipoEquipo.new( un_id, NOMBRE_SINTOMAS[index], IMAGEN_SINTOMAS[index] )
+      
       return equipo
     end
 
