e166f852be1dce24ab77a076aca226e0112a52aa
Alexander <go.alectrico@gmail.com>
2020-04-01 05:44:52 -0300

    Logged_in true

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -4,11 +4,15 @@ class Reader < ApplicationRecord
   has_many   :clients, dependent: :destroy
 
   def logout
+    linea.info "logout"
     update(:logged_in => false)
+    linea.info "logged_in es #{logged_in}"
   end
 
   def login
+    linea.info "login"
     update(:logged_in => true)
+    linea.info "logged_in es #{logged_in}"
   end
 
 end
