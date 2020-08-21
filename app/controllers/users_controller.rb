4fc411e6c4a9fbadf9bb4c8c5e17e657b46084a2
tio <tio@tarara.cl>
2020-08-21 10:46:14 -0400

    Entregando el token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     linea.info "Comando es #{comando.inspect}"
 
     if comando.result
-      json = {:auth_token => token }
+      json = {:auth_token => result }
       render json: json, status: :ok 
       #redirect_to C.admin_login, notice: json
     else
