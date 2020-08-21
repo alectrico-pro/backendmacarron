dfc0ea109e50f55c3106c628596dcf7da19fa157
tio <tio@tarara.cl>
2020-08-21 10:24:28 -0400

    Avanzando

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,17 +17,18 @@ class UsersController < ApplicationController
 
     name_json = { 'name' => name }
 
-    token = CreateUser.call( name_json, user_params[:email], user_params[:password], user_params[:password_confirmation])
+    comando = CreateUser.call( name_json, user_params[:email], user_params[:password], user_params[:password_confirmation])
 
-    linea.info "Token es #{token.inspect}"
-    if token
+    linea.info "Comando es #{comando.inspect}"
+
+    if comando.result
       json = {:auth_token => token }
-      #render json: json, status: :ok 
-      redirect_to C.admin_login, notice: json
+      render json: json, status: :ok 
+      #redirect_to C.admin_login, notice: json
     else
-      json = {"objeto" => "User.create en users_controller #{user.email}","verifyErrors" => user.errors.messages.map{|e| {:name =>e[0], :message => e[1].pop}}}
-      #render json: json, status: :not_found
-      redirect_to C.admin_registro, notice: json
+      json = {"objeto" => "User.create en users_controller #{user.email}","verifyErrors" => comanndo.errors.messages.map{|e| {:name =>e[0], :message => e[1].pop}}}
+      render json: json, status: :not_found
+      #redirect_to C.admin_registro, notice: json
     end
     
 
