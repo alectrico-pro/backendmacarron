4d23ae323eea5b93add6f045c59cad4249db1c75
tio <tio@tarara.cl>
2020-10-29 03:40:12 -0300

    Agregado los temas

---
+++ config/initializers/tema.rb
@@ -0,0 +1,96 @@
+TEMA=ENV['TEMA']
+
+if TEMA == 'COVID'
+
+  ID_SINTOMAS    = [ 100, 101 , 102, 103, 200, 201, 300, 400, 500, 501, 502, 503]
+ 
+  NOMBRE_SINTOMAS = %w(Secreciones_Nasales
+               Dolores_de_Garganta 
+               Pérdida_de_Olfato_y_de_Sabor
+               Problemas_Digestivos
+               Tos_Seca
+               Tos_Seca
+               Fiebre
+               Falta_de_Aire
+               Hipertensión_Arterial
+               Diabetes
+               Asma
+               Cardiopatía   )
+
+  IMAGEN_SINTOMAS = %w(secreciones_nasales_01.jpg
+               dolores_de_garganta_01.jpg
+               sin_olfato_ni_sabor.jpeg
+               problemas_digestivos.jpg
+               tos_03.jpg
+               tos_02.png
+               fiebre_04.jpg
+               taquipnea_03.jpg
+               hipertension_01.jpeg
+               diabetes_01.jpg
+               asma_01.jpg
+               cardiopatia_01.jpg  )
+
+elsif TEMA == 'DESIGNER'
+
+  ID_SINTOMAS      = [ 844, 
+                       99,
+                       1363,
+                       1692,
+                       124,
+                       236,
+                       211,
+                       579,
+                       140,
+                       843,
+                       252,
+                       921,
+                       7,
+                       276,
+                       101,
+                       36,
+                       138,
+                       71,
+                       76]
+
+  NOMBRE_SINTOMAS = %w(Ampolleta Led.
+                       Ampolleta Incandescente.
+                       Licuadora.
+                       Microondas.
+                       Hornillo.
+                       Hervidor.
+                       Olla Arrocera.
+                       Cafetera Expresso.
+                       Plancha.
+                       Frigobar.
+                       Refrigerador de dos puertas.
+                       Lavadora de ropas.
+                       Secadora de ropas.
+                       Lavalozas.
+                       Splitter.
+                       Computador.
+                       Cocina Encimera Vitrocerámica.
+                       Cocina Encimera Inducción.
+                    )
+
+IMAGEN_SINTOMAS =    %w(/img/led.png
+                        /img/ampolleta_incandescente.png
+                        /img/juguera.png
+                        /img/microondas.png
+                        /img/hornillo.png
+                        /img/hervidor.png
+                        /img/olla_arrocera.png
+                        /img/cafetera_expresso.png
+                        /img/plancha.png
+                        /img/frigobar.png
+                        /img/refrigerador.png
+                        /img/refrigerador_dos_puertas.png
+                        /img/lavadora_de_ropa.png
+                        /img/secadora_de_ropa.png
+                        /img/lavaplatos.png
+                        /img/splitter.png
+                        /img/pc.png
+                        /img/encimera.png
+                        /img/encimera_induccion.png
+                     )
+
+end
